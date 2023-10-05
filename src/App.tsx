6d506e05faf5af4d0c8afea5ed5365ba548510c4
dorimu0 <121004915+dorimu0@users.noreply.github.com>
2023-10-05 20:58:22 +0900

    feat: Route & menu 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Map from "./components/Map/Map";
 import Header from "./components/Header/Header";
-// import { Routes, Route, Outlet } from 'react-router-dom'
+import './fonts/font.css'
+import { Routes, Route } from 'react-router-dom'
 
 
 function App() {
@@ -9,6 +10,10 @@ function App() {
     <div className="App">
       <Header />
       <Map />
+
+      <Routes>
+        {/* <Route index element={<HomePage />} />  HomePage컴포넌트 -> album컴포넌트 수정 필요 나머진 알아서하세요  */}
+      </Routes>
 
     </div>
   );
