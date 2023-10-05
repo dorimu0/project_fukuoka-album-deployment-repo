e5c1689f6c0c62c57b438679235bcb7ebbde44f8
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-10-06 00:10:55 +0900

    Add logic to fetch data from JSON server

--- src/components/Album/Album.tsx
+++ src/components/Album/Album.tsx
@@ -1,12 +1,44 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { Post } from "../../types";
 interface Props {
-  // area prop 타입 정의, 선택된 마커의 지역을 받아옴
   area?: string | null;
 }
 
 const Album: React.FC<Props> = ({ area }) => {
-  return <div>{area}</div>; // 선택된 마커의 지역을 출력
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    if (area) {
+      // area가 null이 아닐 때만 요청을 보냄
+      fetch("http://localhost:3004/post")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data); // 받아온 데이터 확인용
+
+          if (data) {
+            const matchedPosts = data.filter(
+              (post: Post) => post.postAreaId === area + "1"
+            );
+            setPosts(matchedPosts);
+          } else {
+            console.error("Unexpected response:", data); // 예상하지 못한 응답 출력
+          }
+        })
+        .catch((error) => console.error("Error:", error)); // 네트워크 요청 중 발생한 에러 출력
+    }
+  }, [area]);
+
+  return (
+    <div>
+      <h1>{area}</h1>
+      {posts.map((post, index) => (
+        <div key={index}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Album;
