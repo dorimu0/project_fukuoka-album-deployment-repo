5d987f37581e3265840ceec19086e0056089af39
Lainari <108247620+Lainari@users.noreply.github.com>
2023-10-07 18:16:27 +0900

    feat: Add logic to fetch data from JSON server

--- src/components/Album/Album.tsx
+++ src/components/Album/Album.tsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Post } from "../../types";
+import AreaPost from '../Post/Post';
 
 interface Props {
   area?: string | null;
 }
 
 const Album: React.FC<Props> = ({ area }) => {
-  return <div>{area}</div>;
-  // Map.tsx에서 가져온 선택된 내용 출력
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    if (area) { // area가 null이 아닐 때만 요청을 보냄
+      fetch('http://localhost:3004/post')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data); // 받아온 데이터 확인용
+  
+          if (data) {
+            const matchedPosts = data.filter((post: Post) => post.postAreaId === area + '1');
+            setPosts(matchedPosts);
+          } else {
+            console.error('Unexpected response:', data); // 예상하지 못한 응답 출력
+          }
+        })
+        .catch(error => console.error('Error:', error)); // 네트워크 요청 중 발생한 에러 출력
+    }
+  }, [area]);
+
+  return (
+    <div>
+      {posts.map((post, index) => (
+        <AreaPost key={index} title={post.title} content={post.content} />
+      ))}
+  </div>
+  );
 };
 
 export default Album;
