d4d65de007a6ca82c13d1833ceecc05c39c90a41
Lainari <108247620+Lainari@users.noreply.github.com>
2023-10-07 20:40:42 +0900

    feat: add feature to load posts by location id with Marker

--- src/components/Album/Album.tsx
+++ src/components/Album/Album.tsx
@@ -1,39 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { Post } from "../../types";
+import { Post as PostType } from "../../types/post.interface";
 import AreaPost from '../Post/Post';
 
 interface Props {
-  area?: string | null;
+  areaId: number;
 }
 
-const Album: React.FC<Props> = ({ area }) => {
-  const [posts, setPosts] = useState<Post[]>([]);
+const Album: React.FC<Props> = ({ areaId }) => {
+  const [posts, setPosts] = useState<PostType[]>([]);
 
   useEffect(() => {
-    if (area) { // area가 null이 아닐 때만 요청을 보냄
-      fetch('http://localhost:3004/post')
-        .then(response => response.json())
-        .then(data => {
-          console.log(data); // 받아온 데이터 확인용
+    fetch('http://localhost:3004/post')
+      .then(response => response.json())
+      .then((data : PostType[]) => {
+        console.log(data); // 받아온 데이터 확인용
   
-          if (data) {
-            const matchedPosts = data.filter((post: Post) => post.postAreaId === area + '1');
-            setPosts(matchedPosts);
-          } else {
-            console.error('Unexpected response:', data); // 예상하지 못한 응답 출력
-          }
-        })
-        .catch(error => console.error('Error:', error)); // 네트워크 요청 중 발생한 에러 출력
-    }
-  }, [area]);
+        if (data) {
+          const matchedPosts = data.filter((post: PostType) => Number(post.postAreaId) === areaId);
+          setPosts(matchedPosts);
+        } else {
+          console.error('Unexpected response:', data); // 예상하지 못한 응답 출력
+        }
+      })
+      .catch(error => console.error('Error:', error)); // 네트워크 요청 중 발생한 에러 출력
+  }, [areaId]);
 
   return (
     <div>
       {posts.map((post, index) => (
-        <AreaPost key={index} title={post.title} content={post.content} />
+        // AreaPost 컴포넌트에게 images prop 전달 
+        <AreaPost key={index} title={post.title} content={post.content} image={post.image}/>
       ))}
-  </div>
-  );
+    </div>
+);
 };
 
 export default Album;
