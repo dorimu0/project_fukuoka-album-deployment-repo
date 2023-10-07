5811816b811e07567c4361ab2eea9529316e579f
Lainari <108247620+Lainari@users.noreply.github.com>
2023-10-07 18:16:13 +0900

    feat: Set up routes in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import Header from "./components/Header/Header";
 import Footer from "./Layout/Footer";
 // import Profile from "./components/Profile/Profile";
 import './fonts/font.css'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Album from "./components/Album/Album";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Map />
-      {/* <Profile /> */}
-      <hr />
-      <Footer />
-      <Routes>
-        {/* <Route index element={<HomePage />} />  HomePage컴포넌트 -> album컴포넌트 수정 필요 나머진 알아서하세요  */}
-      </Routes>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Map />}/>
+          <Route path="/album" element={<Album />} />
+        </Routes>
+        <hr />
+        <Footer />
+        {/* <Router index element={<HomePage />} />  HomePage컴포넌트 -> album컴포넌트 수정 필요 나머진 알아서하세요  */}
+      </Router>
 
     </div>
   );
