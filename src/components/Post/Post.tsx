2781dee3c8d2343cc6f2044871929ee27d607a44
Lainari <108247620+Lainari@users.noreply.github.com>
2023-10-07 20:41:05 +0900

    feat : add image loading feature

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Post } from "../../types";
+import { Post } from "../../types/post.interface";
 import './Post.css';
 
-const Posts: React.FC<Post> = ({ title, content }) => {
-  return (
-    <div className='post'>
-      <h2>{title}</h2>
-      <p>{content}</p>
-    </div>
-  );
+const Posts: React.FC<Post> = ({ title, content, image }) => {
+    // 첫 번째 이미지 선택 
+    const firstImage = image[0];
+  
+    return (
+      <div className='post'>
+        <h2>{title}</h2>
+        <img src={`/img/${firstImage}`} alt={title} /> {/* 이미지 표시 */}
+        <p>{content}</p>
+      </div>
+    );
 };
 
 export default Posts;
