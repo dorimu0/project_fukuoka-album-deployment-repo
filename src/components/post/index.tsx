0e714a06cddb5faab898f0dd264a3f931f36fb5e
Lainari <108247620+Lainari@users.noreply.github.com>
2023-10-08 22:21:46 +0900

    Merge branch 'main' into album

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -1,24 +1,5 @@
 import React from "react";
 import { Post } from "../../types/post.interface";
-<<<<<<< HEAD
-import {PostStyles} from "./PostStyles"
-import { useNavigate } from 'react-router-dom';
-
-const Posts: React.FC<Post> = ({ id, title, content, image }) => {
-    // 첫 번째 이미지 선택 
-    const firstImage = image[0];
-    const navigate = useNavigate();
-
-    const handlePostClick = () => {
-      navigate(`/post/${id}`);
-    };
-  
-    return (
-      <PostStyles className='post' onClick={handlePostClick}>
-        <img src={`${firstImage}`} alt={title} /> {/* 이미지 표시 */}
-      </PostStyles>
-    );
-=======
 import { PostStyles } from "./PostStyles";
 
 const Posts: React.FC<Post> = ({ title, content, image }) => {
@@ -33,7 +14,6 @@ const Posts: React.FC<Post> = ({ title, content, image }) => {
       <img src={`${firstImage}`} alt={title} />
     </PostStyles>
   );
->>>>>>> main
 };
 
 export default Posts;
