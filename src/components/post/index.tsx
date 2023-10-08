22b8d8747f50661e2c785d17adc93540096b0e7a
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-10-08 18:03:41 +0900

    feat: Implement post list

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React from "react";
 import { Post } from "../../types/post.interface";
-import {PostStyles} from "./PostStyles"
+import { PostStyles } from "./PostStyles";
 
 const Posts: React.FC<Post> = ({ title, content, image }) => {
-    // 첫 번째 이미지 선택 
-    const firstImage = image[0];
-  
-    return (
-      <PostStyles className='post'>
-        <h2>{title}</h2>
-        <img src={`/img/${firstImage}`} alt={title} /> {/* 이미지 표시 */}
-        <p>{content}</p>
-      </PostStyles>
-    );
+  // 첫 번째 이미지 선택
+
+  const firstImage = image[0];
+
+  console.log(firstImage);
+
+  return (
+    <PostStyles className="post">
+      <img src={`${firstImage}`} alt={title} />
+    </PostStyles>
+  );
 };
 
 export default Posts;
