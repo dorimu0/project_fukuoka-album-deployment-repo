87f1214a12116dbc846d9c2c438e648edb8ce716
Lainari <108247620+Lainari@users.noreply.github.com>
2023-10-08 21:10:25 +0900

    feat : add navigate to post

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Post } from "../../types/post.interface";
 import {PostStyles} from "./PostStyles"
+import { useNavigate } from 'react-router-dom';
 
-const Posts: React.FC<Post> = ({ title, content, image }) => {
+const Posts: React.FC<Post> = ({ id, title, content, image }) => {
     // 첫 번째 이미지 선택 
     const firstImage = image[0];
+    const navigate = useNavigate();
+
+    const handlePostClick = () => {
+      navigate(`/post/${id}`);
+    };
   
     return (
-      <PostStyles className='post'>
-        <h2>{title}</h2>
-        <img src={`/img/${firstImage}`} alt={title} /> {/* 이미지 표시 */}
-        <p>{content}</p>
+      <PostStyles className='post' onClick={handlePostClick}>
+        <img src={`${firstImage}`} alt={title} /> {/* 이미지 표시 */}
       </PostStyles>
     );
 };
