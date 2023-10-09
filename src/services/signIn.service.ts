64016d3adabfca4a18684d0653ec88ce9cffed85
Z00One <9000248@g.yju.ac.kr>
2023-10-09 23:21:45 +0900

    feat: Update signInByGoogle to dispatch authentication token

--- src/services/signIn.service.ts
+++ src/services/signIn.service.ts
@@ -1,6 +1,7 @@
 import { CredentialResponse } from "@react-oauth/google";
 import { store } from "../store";
 import { setUser } from "../store/user";
+import { setToken } from "../store/token";
 
 export const signInByGoogle = async (googleResponse: CredentialResponse) => {
   const response = await fetch("http://localhost:8000/auth", {
@@ -9,6 +10,8 @@ export const signInByGoogle = async (googleResponse: CredentialResponse) => {
     }
   })
   const userData = await response.json();
+  const { token } = userData;
 
   store.dispatch(setUser(userData));
+  store.dispatch(setToken(token));
 }
