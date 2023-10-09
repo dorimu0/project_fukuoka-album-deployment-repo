28bd3e7c2f0b5afbda84413fcc1a7c4d3099b085
Z00One <9000248@g.yju.ac.kr>
2023-10-09 23:06:48 +0900

    feat(App): add redux store and persistor

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,23 @@ import Modal from "react-modal";
 import Router from "./Router";
 import "./fonts/font.css";
 import Layout from "./components/layout";
+import { Provider } from "react-redux"
+import { persistStore } from "redux-persist"
+import { PersistGate } from "redux-persist/integration/react"
+import { store } from "./store"
 
 Modal.setAppElement("#root");
+const persistor = persistStore(store)
 
 function App(): JSX.Element {
   return (
-    <Layout>
-      <Router />
-    </Layout>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Layout>
+          <Router />
+        </Layout>
+      </PersistGate>
+    </Provider>
   );
 }
 
