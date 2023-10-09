9ccd38395a1fffd1e9c48220a2c4a1c116ed09da
Z00One <9000248@g.yju.ac.kr>
2023-10-09 23:09:34 +0900

    feat: Implement Google OAuth sign-in functionality to Header component

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -16,11 +16,17 @@ import {
   MenuItem,
   FakeBox,
 } from "./HeaderStyles";
+import { RootState, store } from "../../../store";
+import { signInByGoogle } from "../../../services/signIn.service";
+import { useSelector } from "react-redux";
+import { clearUser } from "../../../store/user";
+import { clearToken } from "../../../store/token";
 
 const Header = () => {
   const navigate = useNavigate();
   const [view, setView] = useState<boolean>(false);
   const containerRef = useRef<HTMLDivElement | null>(null);
+  const isSignIn = useSelector((state: RootState) => state.user.isSignIn);
 
   window.addEventListener("mousedown", (event: MouseEvent) => {
     const clickElement = event.target as HTMLElement;
@@ -28,7 +34,7 @@ const Header = () => {
     if (container && view && !container.contains(clickElement)) setView(!view);
   });
 
-  const handleSearchUpdate = (e : React.ChangeEvent<HTMLInputElement>) => {
+  const handleSearchUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value)
   }
 
@@ -39,7 +45,7 @@ const Header = () => {
       </LogoBox>
       <SearchBox>
         <SearchImage src="./search.svg" />
-        <Search type="text" onChange = { handleSearchUpdate }/>
+        <Search type="text" onChange={handleSearchUpdate} />
       </SearchBox>
       <IconBox ref={containerRef}>
         <IconButton
@@ -51,29 +57,42 @@ const Header = () => {
         </IconButton>
         {view ? (
           <Menu>
-            <MenuItem>
-              로그인
-              <FakeBox>
-                <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID as string}>
-                  <GoogleLogin
-                    type={"standard"}
-                    size="medium"
-                    onSuccess={(res) => console.log(res)}
-                  />
-                </GoogleOAuthProvider>
-              </FakeBox>
-            </MenuItem>
-            <MenuItem
-              onClick={() => {
-                navigate("/mypage");
-              }}
-            >
-              내정보
-            </MenuItem>
+            {isSignIn
+              ?
+              <>
+                <MenuItem
+                  onClick={() => {
+                    navigate("/mypage");
+                  }}
+                >
+                  내정보
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    store.dispatch(clearUser())
+                    store.dispatch(clearToken())
+                  }}
+                >
+                  로그아웃
+                </MenuItem>
+              </>
+              : <MenuItem>
+                로그인
+                <FakeBox>
+                  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID as string}>
+                    <GoogleLogin
+                      type={"standard"}
+                      size="medium"
+                      onSuccess={async (res) => {
+                        await signInByGoogle(res);
+                      }
+                      }
+                    />
+                  </GoogleOAuthProvider>
+                </FakeBox>
+              </MenuItem>}
           </Menu>
-        ) : (
-          ""
-        )}
+        ) : null}
       </IconBox>
     </Container>
   );
