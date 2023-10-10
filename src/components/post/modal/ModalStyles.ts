83783eabab7e3e0c868b3802e746dde907317ad6
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-10-10 23:37:14 +0900

    feat : add get user.name & image & style to modal

--- src/components/post/modal/ModalStyles.ts
+++ src/components/post/modal/ModalStyles.ts
@@ -1,44 +1,47 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ModalStyles = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width:100%;
-  height:100%;
-  background-color: rgba(0,0,0,.5);
-  z-index:9999; // 1000으로 되긴 하는데 일단 9999로 고정해주세요
-  
-   .modal-content{
-     position:absolute;
-     top:50%;
-     left:50%;
-     transform:translate(-50%,-50%);
-     background-color:white;
-     max-width:485px;
-     max-height:585px;
-     border-radius:12px;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 9999; // 1000으로 되긴 하는데 일단 9999로 고정해주세요
 
-     .post-image {
-        width:100%;
-        height:100%;
-        object-fit:cover; 
-        border :0.1px solid gray;
-        border-radius:5px;
-        margin:auto;
-      }
+  .modal-content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    max-width: 485px;
+    max-height: 585px;
+    border-radius: 12px;
 
-      img {
-        width:100%;
-        height:100%;
-        margin: 7px;
-        object-fit:cover;
-      }
+    .post-image {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      border: 0.1px solid gray;
+      border-radius: 5px;
+      margin: auto;
+    }
 
-      h3, h2,p {
-        padding-left :10px; 
-      }
-   }
+    img {
+      width: 100%;
+      height: 100%;
+      max-height: 400px;
+      margin: 7px;
+      object-fit: contain;
+    }
+
+    h3,
+    h2,
+    p {
+      padding-left: 10px;
+    }
+  }
 `;
 
 export const Icon = styled.img`
