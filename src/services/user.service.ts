ac5660ad3cbfe6f5aae75d549a25e8a5929b5df0
Z00One <9000248@g.yju.ac.kr>
2023-10-11 23:00:22 +0900

    feat: Add user authentication process to user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,10 @@
 import { User, UserUpdate } from "../types/user.interface";
+import { verify } from "./auth.service";
 
-export const getUser = async (id: string): Promise<User> => {
+export const getUser = async (id: number | null): Promise<User> => {
+  if (id === null) {
+    window.alert("유저 정보를 불러올 수 없습니다. 다시 로그인하세요.")
+  }
   const res = await fetch(`http://localhost:3004/user/${id}`);
 
   if (!res.ok) {
@@ -11,7 +15,12 @@ export const getUser = async (id: string): Promise<User> => {
   return user;
 };
 
-export const updateUser = async (user: UserUpdate): Promise<User> => {
+export const updateUser = async (user: UserUpdate): Promise<User | undefined> => {
+  const auth = await verify();
+  if (!auth) {
+    return;
+  }
+
   const res = await fetch(`http://localhost:3004/user/${user.id}`, {
     method: "PUT",
     headers: {
