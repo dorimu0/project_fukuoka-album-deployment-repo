fdb09b6f387d2ac05a7af5c8ea216ce3f851f004
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-10-11 17:38:24 +0900

    feat: Implement post opacity

--- src/components/post/PostStyles.ts
+++ src/components/post/PostStyles.ts
@@ -2,8 +2,13 @@ import styled from "styled-components";
 
 export const PostStyles = styled.div`
   width: 250px;
+  height: 200px;
+  border-radius: 10px;
   border: 11px solid white;
   padding: 0;
   margin-right: 10px;
   margin-bottom: 10px;
+  &:hover {
+    opacity: 0.6;
+  }
 `;
