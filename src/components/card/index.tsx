e5dc150c72a651d55398c4aa996b5ef4f8e44808
ユミン <55650732+yuminn-k@users.noreply.github.com>
2023-10-12 20:54:36 +0900

    refactor: rename Card to card

---
+++ src/components/card/index.tsx
@@ -0,0 +1,37 @@
+import Modal from "../modal";
+import {
+  CardWrapper,
+  CardImage,
+  CardTextWrapper,
+  CardTextTitle,
+  CardTextBody,
+} from "./CardStyles";
+import { User } from "../../types/user.interface";
+
+export default function Card({
+  user,
+  onUserUpdated,
+}: {
+  user: User;
+  onUserUpdated: (user: User) => void;
+}) {
+  return (
+    <>
+      <CardWrapper>
+        <CardImage background={user.imageUrl} />
+        <CardTextWrapper>
+          <CardTextTitle>{user.name}</CardTextTitle>
+          <Modal
+            id={user.id}
+            email={user.email}
+            name={user.name}
+            comment={user.comment}
+            imgUrl={user.imageUrl}
+            onUserUpdated={onUserUpdated}
+          />
+          <CardTextBody>{user.comment}</CardTextBody>
+        </CardTextWrapper>
+      </CardWrapper>
+    </>
+  );
+}
