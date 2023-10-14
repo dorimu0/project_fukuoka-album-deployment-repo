7e5f39eb7ed25039f32bc993876c476a473dc74d
Z00One <9000248@g.yju.ac.kr>
2023-10-14 21:05:07 +0900

    fix: Fix getUser function's argument in useEffect hook

--- src/components/post/modal/index.tsx
+++ src/components/post/modal/index.tsx
@@ -14,7 +14,7 @@ const Modal: React.FC<ModalProps> = ({ post, onClose }) => {
   const [user, setUser] = useState({ name: '', imageUrl: '' });
 
   useEffect(() => {
-    getUser(String(post.userId))
+    getUser(post.userId)
     .then(user => setUser({ name: user.name, imageUrl: user.imageUrl }))
       .catch(err => console.error(err));
   }, [post.userId]);
