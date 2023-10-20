bdd773b5105dc2642edc2e2269ec585c3304e09e
Z00One <9000248@g.yju.ac.kr>
2023-10-20 11:36:48 +0900

    fix: Fix fetchOptions function

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,30 +1,29 @@
 import { store } from "../store";
-import { refresh, verify } from "./auth.service";
+import { refresh } from "./auth.service";
 
 type FetchMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 
 export const fetchOptions = (
-  method: string,
-  body: object | null,
-  includeImage: boolean = false
+  method: FetchMethod,
+  body: object | null | undefined = undefined
 ) => {
   const token = store.getState().token.accessToken || "";
 
-  const headers: HeadersInit = {
-    Authorization: token,
-  };
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", token);
 
   if (body) {
-    headers["Content-Type"] = "application/json";
-  }
-  if (includeImage) {
-    headers["Content-Type"] = "multipart/form-data";
+    myHeaders.append("Content-Type", "application/json");
+    return {
+      headers: myHeaders,
+      method,
+      body: body ? JSON.stringify(body) : null,
+    };
   }
 
   return {
     method,
-    headers,
-    body: body ? JSON.stringify(body) : null,
+    headers: myHeaders,
   };
 };
 
@@ -34,11 +33,14 @@ export const api = async (
   body: object | null = null
 ) => {
   const options = fetchOptions(method, body);
+
   const url = `http://localhost:8000/${endpoint}`;
 
   let res = await fetch(url, options);
 
-  if (res.status === 410 || res.status === 410) {
+  // 인증 기간이 지났을 경우
+  if (res.status === 410) {
+    // 리프레쉬가 이뤄지지 않으면 로그아웃 됨
     const auth = await refresh();
     if (!auth) {
       return;
@@ -47,7 +49,9 @@ export const api = async (
     res = await fetch(url, options);
   }
 
-  return await res.json();
+  const data = await res.json();
+
+  return data;
 };
 
 export const uploadApi = async (
