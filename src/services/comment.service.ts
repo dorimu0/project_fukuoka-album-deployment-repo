17cf74fd0877db32f3e43ff56680c66c99a12332
Z00One <9000248@g.yju.ac.kr>
2023-10-23 23:26:41 +0900

    refactor: Refactor existing functions to use the api service for making requests

--- src/services/comment.service.ts
+++ src/services/comment.service.ts
@@ -1,79 +1,43 @@
-import { CommentInterface } from '../types/comment.interface';
-import { Post as PostType } from '../types/post.interface';
+import { CommentInterface } from "../types/comment.interface";
+import { Post as PostType } from "../types/post.interface";
+import { api } from "./api.service";
 
-export const getCommentsByPostId = async (postId: number): Promise<CommentInterface[]> => {
-    const response = await fetch(`http://localhost:3004/comment?postId=${postId}`);
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return await response.json();
+export const getCommentsByPostId = async (
+  postId: number
+): Promise<CommentInterface[]> => {
+  const comments = await api("GET", `comment?postId=${postId}`);
+
+  return comments;
 };
 
-export const createComment = async (comment: CommentInterface): Promise<CommentInterface> => {
-    const res = await fetch(`http://localhost:3004/comment`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(comment)
-    });
-  
-    if (!res.ok) {
-      throw new Error("에러 발생!");
-    }
-  
-    const createdComment = await res.json();
-  
-    return createdComment;
-};
+export const createComment = async (
+  comment: CommentInterface
+): Promise<CommentInterface> => {
+  const createdComment = await api("POST", `comment`, comment);
 
+  return createdComment;
+};
 
 export const deleteComment = async (id: number): Promise<void> => {
-  const res = await fetch(`http://localhost:3004/comment/${id}`, {
-    method: 'DELETE',
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to delete comment");
-  }
+  await api("DELETE", `comment/${id}`);
 };
 
-export const updateComment = async (comment: CommentInterface): Promise<CommentInterface> => {
-  const res = await fetch(`http://localhost:3004/comment/${comment.id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(comment)
-  });
-  
-  if (!res.ok) {
-    throw new Error("Failed to update comment");
-  }
+export const updateComment = async (
+  comment: CommentInterface
+): Promise<CommentInterface> => {
+  const updatedComment = await api("PUT", `comment/${comment.id}`, comment);
 
-  return await res.json();
+  return updatedComment;
 };
-  
+
 export const updateCommentId = async (post: PostType): Promise<PostType> => {
-  const res = await fetch(`http://localhost:3004/post/${post.id}`,{
-      method: 'PUT',
-      headers:{
-          'Content-Type':'application/json'
-      },
-      body: JSON.stringify(post)
-  });
+  const updatedCommentId = await api("PUT", `post/${post.id}`, post);
 
-  if (!res.ok){
-      throw new Error("Network response was not ok");
-  }
-
-  return await res.json();
-}
+  return updatedCommentId;
+};
 
 export const getPost = async (id: number): Promise<PostType> => {
-  const res = await fetch(`http://localhost:3004/post/${id}`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch post");
-  }
-  return await res.json();
+  const post = await api("GET", `post/${id}`);
+
+  return post;
 };
