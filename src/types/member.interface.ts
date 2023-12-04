3557d2ae89895dee9f22ce0766d8b98db47b4666
Z00One <9000248@g.yju.ac.kr>
2023-12-04 14:45:58 +0900

    feat(member): add CreateMember type

--- src/types/member.interface.ts
+++ src/types/member.interface.ts
@@ -2,10 +2,14 @@ export type Member = {
   id: number;
   name: string;
   position: string;
-  imageUrl?: string;
+  imageUrl: string;
 };
 
-export type MemberWithoutId = Omit<Member, "id">;
+export type CreateMember = {
+  name: string;
+  position: string;
+  imageUrl?: string;
+};
 
 export interface myImage {
   image: string[];
